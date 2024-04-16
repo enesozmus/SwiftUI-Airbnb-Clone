7da03c294d1ff61c6b9d014422076ed8f3b40923
enesozmus <ensozmus@gmail.com>
2024-04-16 16:30:41 +0300

    Initial Commit

---
+++ SwiftUI-Airbnb-Clone/SwiftUI_Airbnb_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Airbnb_CloneApp.swift
+//  SwiftUI-Airbnb-Clone
+//
+//  Created by enesozmus on 16.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Airbnb_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
