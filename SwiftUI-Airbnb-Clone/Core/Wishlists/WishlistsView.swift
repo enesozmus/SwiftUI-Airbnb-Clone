935ca3f27d9cf66765e5fbcb81507362870446b1
enesozmus <ensozmus@gmail.com>
2024-04-17 15:17:48 +0300

    Wish Lists View

---
+++ SwiftUI-Airbnb-Clone/Core/Wishlists/WishlistsView.swift
@@ -0,0 +1,41 @@
+//
+//  WishlistsView.swift
+//  SwiftUI-Airbnb-Clone
+//
+//  Created by enesozmus on 17.04.2024.
+//
+
+import SwiftUI
+
+struct WishlistsView: View {
+    var body: some View {
+        NavigationStack {
+            VStack(alignment: .leading, spacing: 32) {
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Log in to view your wishlists")
+                        .font(.headline)
+                    Text("You can create, view or edit wishlists once you have logged in")
+                        .font(.footnote)
+                }
+                Button {
+                    print("log in")
+                } label: {
+                    Text("Log in")
+                        .foregroundStyle(.white)
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .frame(width: 360, height: 48)
+                        .background(.pink)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                }
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("Wishlists")
+        }
+    }
+}
+
+#Preview {
+    WishlistsView()
+}
